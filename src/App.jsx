e1577766426ab19c171588910f98a38fd955282f
vishwanath <vishwanathittangi2006@gmail.com>
2026-08-30 20:03:26 +0530

    Fix router basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from "./pages/Contact.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio4">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
